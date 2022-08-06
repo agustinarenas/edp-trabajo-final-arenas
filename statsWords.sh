64f5b02c7a18536d425c280a6073baa752ba64cf
Agustin <agustinarenas1803@gmail.com>
2022-08-06 20:08:36 -0300

    ejercicio-1 Probar la ejecucion del script
    Ver si detecta correctamente la palabra mas larga y la mas corta.

--- statsWords.sh
+++ statsWords.sh
@@ -1,3 +1,25 @@
 #!/bin/bash 
 
+f=$(head $1)
+count=0
+p_l=0
+p_c=1000
+long=0
+for i in $f
+do
+	count=$((count+1))
+	n_carac=${#i}
+	long=$((n_carac+long))
+	if [ $n_carac -gt $p_l ]; then
+		p_l=$n_carac
+		palabra_l=$i
+	fi
+        if [ $n_carac -lt $p_c ]; then
+                p_c=$n_carac
+                palabra_c=$i
+        fi
 
+done
+echo "$p_l $palabra_l"
+echo "$p_c $palabra_c"
+exit 0
