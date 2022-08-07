7023b26bfcb08feffd79f96b1fc61ced31879c3c
Agustin <agustinarenas1803@gmail.com>
2022-08-07 14:03:37 -0300

    ejercicio-5 Finalizar
    Se agrego el calculo de lineas en blanco y se definio la muestra del resultado.

--- blankLinesCounter.sh
+++ blankLinesCounter.sh
@@ -2,9 +2,17 @@
 
 [[ ! -e $1  || $# -ne 1 ]] && echo "El archivo no existe o no ingreso un argumento" && exit 1
 
-wc -l < $1
+l_t=$(wc -l < $1)
 
 grep -v '^$' $1 > filetemporal.txt
-cat filetemporal.txt
 
-wc -l < filetemporal.txt
+l_sb=$(wc -l < filetemporal.txt)
+
+rm filetemporal.txt
+
+c_lb=$((l_t-l_sb))
+
+
+echo "La cantidad de lineas en blanco que posee el archivo es: $c_lb"
+
+exit 0
