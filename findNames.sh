ae458d3265e8306fd3824d3619e21b9c4f28a139
Agustin <agustinarenas1803@gmail.com>
2022-08-07 01:28:24 -0300

    ejercicio-2 Prueba de funcionamiento
    Se realizo el ejercicio, se itero en prueba y error y se agrego las condiciones para los argumentos.

---
+++ findNames.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+[[ ! -e $1  || $# -ne 1 ]] && echo "El archivo no existe o no ingreso un argumento" && exit 1
+
+f=$(cat $1)
+regex="^[A-Z][a-z]*$"
+echo "Las palabras con mayuscula en el archivo son:"
+for i in $f
+do
+	c_esp=${i: -1}
+	case $c_esp in 
+		',' | '.' | ';' | ':')
+			i=${i:0:-1}
+		;;
+	esac
+
+	if [[ "${i}" =~ ${regex} ]]; then
+		echo $i >> filetemporal.txt	
+	fi
+done
+cat filetemporal.txt | sort | uniq
+rm filetemporal.txt
+
+exit 0
